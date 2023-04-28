248a91718939a63a60f0e6fe1f0e75b261e4e146
Luki20091 <76406872+Luki20091@users.noreply.github.com>
2023-04-28 09:41:06 +0200

    Add files via upload

---
+++ ConsoleApp16/Class/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16.Class
+{
+    public class Student : Uzytkownik
+    {
+        public int Rocznik { get; set; }
+        public Student(string imie, string nazwisko, int rocznik) : base(imie, nazwisko)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Rocznik = rocznik;
+        }
+        public override string ToString()
+        {
+            return $"Login: {Login} \nHasło: {ZaszyfrujGADERYPOLUKI(Haslo)}";
+        }
+        public override bool Equals(Object obj)
+        {
+            if (!(obj is Student)) 
+                return false; 
+            return true;
+        }
+    }
+}
---
+++ ConsoleApp16/Class/Uzytkownik.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16.Class
+{
+    public class Uzytkownik : Szyfrowanie
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Login { get { return Imie + "_" + Nazwisko; } set { Login = Imie + "_" + Nazwisko; } }
+        public string Haslo { get { Random rnd = new Random(); return Imie.Substring(0,3) + Convert.ToString(rnd.Next(100000, 999999)); } set { Random rnd = new Random(); Haslo = Imie[0] + Imie[1] + Imie[2] + Convert.ToString(rnd.Next(100000, 999999)); } }
+
+        public Uzytkownik(string imie, string nazwisko) {
+            Imie = imie;
+            Nazwisko = nazwisko;
+        }
+
+        public string UkryjLogin()
+        {
+            return ZaszyfrujGADERYPOLUKI(Login);
+        }
+        public string UkryjHaslo()
+        {
+            return ZaszyfrujGADERYPOLUKI(Haslo);
+        }
+        public string PokazLogin()
+        {
+            return OdszyfrujGADERYPOLUKI(Login);
+        }
+        public string PokazHaslo()
+        {
+            return OdszyfrujGADERYPOLUKI(Haslo);
+        }
+
+
+        public void ZmienLogin()
+        {
+            Console.WriteLine("\nPodaj stary login:");
+            string staryLogin = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Podaj nowy login:");
+            string nowyLogin = Convert.ToString(Console.ReadLine());
+            if (Login == staryLogin)
+                Login = nowyLogin;
+        }
+        public void ZmienHaslo()
+        {
+            Console.WriteLine("\nPodaj stare hasło:");
+            string stareHaslo = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Podaj nowe hasło:");
+            string noweHaslo = Convert.ToString(Console.ReadLine());
+            if (Haslo == stareHaslo)
+                Haslo = noweHaslo;
+        }
+    }
+}
